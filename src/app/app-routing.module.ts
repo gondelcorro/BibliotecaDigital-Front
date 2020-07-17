c6425f3df78aaa5f70ac8a5a27a6d56cd2e84750
gondelcorro <delcorrog@gmail.com>
2020-07-16 22:12:49 -0300

    Add useHash in app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})], //El use # lo agrego como estrategia en las URL
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
